217f554cacf704f8292b2a93427ad83596afe583
sunrabbit123 <qudwls185@naver.com>
2021-07-16 07:38:52 +0900

    feat : .vscode ignore && MISC : information.json -> js

---
+++ information.js
@@ -0,0 +1,13 @@
+export const student = [
+  { "1st": {} },
+  { "2nd": {} },
+  { "3rd": {} },
+  {
+    "4th": [
+      {
+        name: "오병진",
+        nickname: "sunrabbit123",
+      },
+    ],
+  },
+];
