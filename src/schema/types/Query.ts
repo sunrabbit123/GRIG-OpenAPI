595fd9eca87fb9b39074d763bab5a4d7ed3038e2
sunrabbit123 <qudwls185@naver.com>
2021-07-15 14:15:30 +0900

    feat : query setting

--- src/schema/types/Query.ts
+++ src/schema/types/Query.ts
@@ -1,5 +1,9 @@
 import { queryType } from "nexus";
+import * as q from "./queries";
 
 export const Query = queryType({
-  definition(t) {},
+  definition(t) {
+    t.field("user", q.user);
+    t.list.field("ranking", q.ranking);
+  },
 });
