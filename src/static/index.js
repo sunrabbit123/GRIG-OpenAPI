4ac1771d14ba400fcc557fb6f972a5c24d66ffa6
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-07-28 16:52:37 +0900

    update : Minimize overlap
    중복을 줄였고,
    js에 있던 버그도 해결했습니다.

--- src/static/index.js
+++ src/static/index.js
@@ -1,32 +1,30 @@
 const validTime = document.querySelector('.valid-time');
 const validBtn = document.querySelector('.valid-btn');
-const formInput = document.querySelector('.form-input')
+const formInput = document.querySelector('.form-input');
+const sendMessage = document.querySelector('.send');
 
 let disabled = true
 let timer;
+let loop;
 
 function Timer() {
     validTime.innerText = `${Math.floor(timer / 60)}분 ${timer % 60}초`;
     timer -= 1;
-    if (timer <= 0) {
+    if (timer < 0) {
         validTime.style.display = "none"
+        sendMessage.style.display = "none";
         clearInterval(loop);
-        validBtn.disabled = !disabled;
-        formInput.disabled = !disabled;
-        validBtn.style.cursor = "pointer"
     }
 }
 
 validBtn.addEventListener("click", e => {
     e.preventDefault();
-    
-    validBtn.disabled = disabled;
-    formInput.disabled = disabled;
 
-    validTime.style.display = "block"
-    validBtn.style.cursor = "auto"
+    validTime.style.display = "block";
+    sendMessage.style.display = "block";
 
     timer = 60 * 5;
+    if (loop) clearInterval(loop);
 
     Timer();
     loop = setInterval(Timer, 1000);
