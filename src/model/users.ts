2ce24e584aa17b9b4f117eef6c480d6579149b80
sunrabbit123 <qudwls185@naver.com>
2021-08-25 20:34:18 +0900

    update: 랭킹 조회시 인증된 유저만 가능하도록 수정

--- src/model/users.ts
+++ src/model/users.ts
@@ -167,7 +167,7 @@ export class Users {
     this: ModelType<Users> & typeof Users,
     options: INFORMATION_DTO.GetRankingInput
   ): Promise<Array<DocumentType<Users>>> {
-    const userList = await this.find()
+    const userList = await this.find({ certified: true })
       .sort(INFORMATION_DTO.RankingSortCriteria[options.criteria])
       .skip((options.page - 1) * options.count)
       .limit(options.count)
