2c70271c31b0f46264e3080bfc9a49fc497ca0ec
sunrabbit123 <qudwls185@naver.com>
2021-12-11 14:37:42 +0900

    update delete scripts

--- util/code.ts
+++ util/code.ts
@@ -2,17 +2,11 @@ import * as mongoose from "mongoose";
 import { CodeModel } from "../src/model/code";
 
 export const deleteRemainCode: Function = async (): Promise<void> => {
-  const db = await mongoose.connect(process.env.MongoDBUrl ?? "", {
-    useFindAndModify: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  });
+  
 
   var dateTime = new Date();
   dateTime.setMinutes(dateTime.getMinutes() - 5);
   await CodeModel.deleteMany({ createdAt: { $lte: dateTime } });
   console.log("사용되지않는 코드들 제거 완료");
-  db.disconnect();
   return;
 };
--- util/delete.ts
+++ util/delete.ts
@@ -1,9 +1,18 @@
+import { mongoose } from "@typegoose/typegoose";
 import { deleteRemainCode } from "./code";
 import { deleteRemainNotCertifiedUser } from "./user";
 
 async function deleteRemainDocument(): Promise<void> {
+  const db = await mongoose.connect(process.env.MongoDBUrl ?? "", {
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   await deleteRemainNotCertifiedUser();
   await deleteRemainCode();
+
+  await db.disconnect();
   return;
 }
 
